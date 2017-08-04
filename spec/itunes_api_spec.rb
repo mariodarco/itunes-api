11a6084422b6e05007e47defc0e894111c028154
Mario D’Arco <mario.darco@onthebeach.co.uk>
2017-08-04 18:18:55 +0100

    Complete test suite for ItunesApi module

--- spec/itunes_api_spec.rb
+++ spec/itunes_api_spec.rb
@@ -8,43 +8,69 @@ describe ItunesApi do
   it 'has a limit' do
     expect(ItunesApi::LIMIT).to eq 200
   end
+end
+
+describe ItunesApi do
+  let(:name) { 'The Beatles' }
+  let(:search_class) { ItunesApi::Requests::Search }
+  let(:store) { :it }
 
   describe '.artist_ids' do
     subject { described_class.artist_ids(name, store) }
 
-    let(:name) { 'The Beatles' }
-    let(:store) { :it }
+    it 'delegates the search to the Request::Search class' do
+      expect(search_class).to receive(:artist_ids).with(name, store)
+
+      subject
+    end
+
+    context 'when no store is specified' do
+      subject { described_class.artist_ids(name) }
+
+      it 'performs the search on the default store' do
+        expect(search_class).to receive(:artist_ids).with(name, :gb)
+
+        subject
+      end
+    end
+  end
+
+  describe '.artist_data' do
+    subject { described_class.artist_data(name, store) }
 
     it 'delegates the search to the Request::Search class' do
-      expect(ItunesApi::Requests::Search).to receive(:artist_ids).with(name, store)
+      expect(search_class).to receive(:artist_data).with(name, store)
+
+      subject
+    end
+  end
+end
+
+describe ItunesApi do
+  let(:artist_class) { ItunesApi::Music::Artist }
+  let(:store) { :gb }
+
+  describe '.all_artists_by_name' do
+    subject { described_class.all_artists_by_name(name, store) }
+
+    let(:name) { 'Pink Floyd' }
+
+    it 'delegates the search to the Music::Artist class' do
+      expect(artist_class).to receive(:search).with(name, store)
 
       subject
     end
   end
 
-  # describe '.artist_lookup' do
-  #   subject { described_class.artist_lookup(id, store) }
+  describe '.artist_lookup' do
+    subject { described_class.artist_lookup(apple_id, store) }
 
-  #   let(:id) { '5555' }
-  #   let(:store) { :it }
+    let(:apple_id) { 999 }
 
-  #   it 'delegates the ArtistLookup class to retrieve artist from the query' do
-  #     expect(ItunesApi::Music::ArtistLookup).to receive(:artist).with(id, store)
+    it 'delegates the search to the Music::Artist class' do
+      expect(artist_class).to receive(:find_by_apple_id).with(apple_id, store)
 
-  #     subject
-  #   end
-  # end
-
-  # describe '.artist_search' do
-  #   subject { described_class.artist_search(name, store) }
-
-  #   let(:name) { 'The Beatles' }
-  #   let(:store) { :it }
-
-  #   it 'delegates the ArtistSearch class to retrieve artists from the query' do
-  #     expect(ItunesApi::Music::ArtistSearch).to receive(:artists).with(name, store)
-
-  #     subject
-  #   end
-  # end
+      subject
+    end
+  end
 end
