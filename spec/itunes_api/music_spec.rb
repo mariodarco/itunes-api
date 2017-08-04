de9a9e4b809c646a27a73c5aca6f539e30a25bc1
Mario D’Arco <mario.darco@onthebeach.co.uk>
2017-08-04 23:16:44 +0100

    Tests for the Music module

---
+++ spec/itunes_api/music_spec.rb
@@ -0,0 +1,52 @@
+require 'spec_helper'
+
+describe ItunesApi::Music do
+  let(:name) { 'Metallica' }
+  let(:store) { :it }
+
+  describe '.all_apple_ids' do
+    subject do
+      VCR.use_cassette('search') { described_class.all_apple_ids(name, store) }
+    end
+
+    let(:apple_ids) do
+      [3996865, 84240723, 295913859, 346711868, 876367086, 407152456, 293610693]
+    end
+
+    it { is_expected.to eql apple_ids }
+  end
+
+  describe '.all_artists_by_name' do
+    subject do
+      VCR.use_cassette('search') do
+        described_class.all_artists_by_name(name, store)
+      end
+    end
+
+    it { is_expected.to be_a(Array) }
+    it { is_expected.to all be_a(ItunesApi::Music::Artist) }
+
+    it 'has relevant results' do
+      expect(subject.map(&:name)).to all be_include(name)
+    end
+  end
+end
+
+describe ItunesApi::Music do
+  let(:apple_id) { 3996865 }
+  let(:store) { :gb }
+
+  describe '.find_by_apple_id' do
+    subject do
+      VCR.use_cassette('lookup') do
+        described_class.find_by_apple_id(apple_id, store)
+      end
+    end
+
+    it { is_expected.to be_a(ItunesApi::Music::Artist) }
+
+    it 'is the expected result' do
+      expect(subject.apple_id).to eql apple_id
+    end
+  end
+end
