82128eabfda8bec7fb16d19071c09fcd551d9c11
Mario D'Arco <mario.darco.78@gmail.com>
2019-12-27 22:53:32 +0000

    lint Ruby files

--- spec/itunes_api/music/requests/artist_spec.rb
+++ spec/itunes_api/music/requests/artist_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 RSpec.describe ItunesApi::Music::Requests::Artist, type: :model do
--- spec/itunes_api/music/song_spec.rb
+++ spec/itunes_api/music/song_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 RSpec.describe ItunesApi::Music::Song, type: :model do
